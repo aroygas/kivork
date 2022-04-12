992514c9c58af15abbe04b588762128a6731d992
Aleksandr Roigas <aroigas@amfam.com>
2022-04-12 20:56:15 +0400

    Added third task

--- src/test/java/com/aroygas/kivork/KivorkTest.java
+++ src/test/java/com/aroygas/kivork/KivorkTest.java
@@ -7,15 +7,20 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.WebDriverRunner.*;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 
+
 public class KivorkTest {
 
-    @Test (groups = {"web"})
+    @Test (groups = {"web", "stale"})
     @Parameters({"url"})
     public void StaleElementTest (String url) {
         //There should be some reasonable limit for amount of tries to get stale element back
@@ -50,5 +55,19 @@ public class KivorkTest {
         //c) Assert your latitude and longitude with a 0.01° tolerance
         Assert.assertEquals(response.jsonPath().getFloat("longitude"),  longitude, 0.01);
         Assert.assertEquals(response.jsonPath().getFloat("latitude"),  latitude, 0.01);
+    }
+
+    @Test (groups = {"web", "newtab"})
+    @Parameters({"url"})
+    public void SwitchTabsTest (String url) {
+        Selenide.open(url);
+        $(byText("Get Free Apikey")).click(); //Click on button that opens new tab
+
+        List<String> tabs = new ArrayList<String>(getWebDriver().getWindowHandles());
+        Selenide.switchTo().window(tabs.get(1)); // Switching to new tab
+        $("title").shouldHave(attribute("text", "freegeoip"));//Assert page title
+        Selenide.switchTo().window(tabs.get(0)); // Switching to old tab
+        $("title").shouldHave(attribute("text",
+                "Free IP Geolocation API - FreeGeoIP.app"));//Assert page title
     }
 }
