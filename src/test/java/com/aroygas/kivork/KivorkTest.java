03eb617920810ff536cc8e19f30f61ed17bf79cf
Aleksandr Roigas <aroigas@amfam.com>
2022-04-12 20:19:01 +0400

    Added second task

--- src/test/java/com/aroygas/kivork/KivorkTest.java
+++ src/test/java/com/aroygas/kivork/KivorkTest.java
@@ -1,17 +1,21 @@
 package com.aroygas.kivork;
 
 import com.codeborne.selenide.Selenide;
+import io.restassured.response.Response;
 import org.openqa.selenium.StaleElementReferenceException;
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 
 public class KivorkTest {
 
-    @Test
+    @Test (groups = {"web"})
     @Parameters({"url"})
     public void StaleElementTest (String url) {
         //There should be some reasonable limit for amount of tries to get stale element back
@@ -33,5 +37,18 @@ public class KivorkTest {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    @Test (groups = {"api"})
+    @Parameters({"url", "longitude", "latitude"})
+    public void APICallTest (String url, float longitude, float latitude) {
+        Response response = given().
+                contentType(JSON).
+                get(url + "json");
+        response.then().assertThat().statusCode(200); //a) Assert the response code;
+        response.getBody().prettyPrint(); //b) Parse the response;
+        //c) Assert your latitude and longitude with a 0.01° tolerance
+        Assert.assertEquals(response.jsonPath().getFloat("longitude"),  longitude, 0.01);
+        Assert.assertEquals(response.jsonPath().getFloat("latitude"),  latitude, 0.01);
     }
 }
